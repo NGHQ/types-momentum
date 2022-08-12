4e59cbf6bfcf4c5fcb88a849eaff5f364546f0c2
laffed <roarklaffed@gmail.com>
2022-08-12 17:33:28 -0500

    .

--- src/methods/utility.ts
+++ src/methods/utility.ts
@@ -1,7 +1,6 @@
-  export const getFlavoredValue = <O extends object>(
-    object: O, id: keyof O
-  ): O[keyof O] | undefined => {
-  
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions --  okay
-    return object[id] as O[keyof O] | undefined;
-  };
+export const getFlavoredValue = <O extends Record<string, unknown>>(
+  object: O, id: keyof O
+): O[keyof O] | undefined => {
+
+  return object[id] as O[keyof O] | undefined;
+};
