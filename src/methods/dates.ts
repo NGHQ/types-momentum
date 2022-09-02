a18a3b0b78eaeda6ff9265aad68f0e92627fb204
laffed <roarklaffed@gmail.com>
2022-09-02 15:45:30 -0500

    .

--- src/methods/dates.ts
+++ src/methods/dates.ts
@@ -2,11 +2,22 @@ import dayjs from "dayjs";
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 
-import type { UnNullableTimestamp, TZCode } from "../types";
+import type { UnNullableTimestamp, TZCode, Timestamp as NullableTimestamp } from "../types";
 import { Timestamp } from '@firebase/firestore';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+
+/**
+ * @description Allows passing IANA Timezone code not listed in TZCode union. Should be used only for dynamically generated codes. 
+ * */
+ export const stringIsIanaTimezone = (str: string): TZCode => {
+  const predicateStringIsIanaTimezone = (str: string): str is TZCode => typeof str === 'string';
+
+  if (predicateStringIsIanaTimezone(str)) return str;
+
+  return 'America/New_York';
+}
 
 export const daysDiffFromNow = (
   now: UnNullableTimestamp,
@@ -48,13 +59,39 @@ export const streakStateAfterCompletion = (
   }
 }
 
-/**
- * @description Allows passing IANA Timezone code not listed in TZCode union. Should be used only for dynamically generated codes. 
- * */
-export const stringIsIanaTimezone = (str: string): TZCode => {
-  const predicateStringIsIanaTimezone = (str: string): str is TZCode => typeof str === 'string';
 
-  if (predicateStringIsIanaTimezone(str)) return str;
+export const formatCreationTimestamp = (
+  createdAt: NullableTimestamp,
+  maxDaysDisplay: number = 6 
+): string => {
+  if (createdAt === null) return '';
 
-  return 'America/New_York';
+  const now = dayjs();
+  const comparator = dayjs(createdAt.toMillis());
+  const fullDate = comparator.format('MMM D, YYYY');
+
+  if (now.diff(comparator, 'd') > maxDaysDisplay) {
+    return fullDate;
+  }
+
+  const diffs = [
+    ['d', now.diff(comparator, 'd')], 
+    ['h', now.diff(comparator, 'h')], 
+    ['m', now.diff(comparator, 'm')], 
+  ] as const;
+
+  let format: undefined | typeof diffs[number];
+
+  for (const diff of diffs) {
+    if (diff[1]) {
+      format = diff;
+      break;
+    }
+  }
+
+  if (format === undefined) return 'now'; 
+
+  const [postfix, count] = format;
+  
+  return `${count}${postfix} ago`;
 }
