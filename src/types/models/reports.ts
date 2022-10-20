70b5304944381048a042ee8a2f476e700db0040e
laffed <roarklaffed@gmail.com>
2022-10-20 13:49:15 -0500

    .

--- src/types/models/reports.ts
+++ src/types/models/reports.ts
@@ -1,9 +1,16 @@
-import { CommunityContentId, Timestamp, UserId } from "../utility"
+import { CommunityContentId, CommunityId, Timestamp, UserId } from "../utility"
 
 export type ReportDocumentData = {
   reporter: UserId;
   reportee: UserId;
-  reportedContentId?: CommunityContentId;
   description: string;
   createdAt: Timestamp;
-}
+} & ({
+  tag: 'content';
+  reportedContent: CommunityContentId;
+  communityId: CommunityId;
+} | {
+  tag: 'user';
+  reportedContent: never;
+  communityId: never;
+})
