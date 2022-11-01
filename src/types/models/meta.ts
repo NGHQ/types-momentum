c1c2b19b4564791f61f2947465a26919adeeeb47
laffed <roarklaffed@gmail.com>
2022-11-01 12:27:22 -0500

    version meta device specific

--- src/types/models/meta.ts
+++ src/types/models/meta.ts
@@ -1,12 +1,16 @@
 export type MetaDocumentId = |
   'version' | 'onboarding'
 
-export type VersionMetaDocumentData = {
-  minimumVersion: string;
-  latestVersion: string;
-  currentMajor: number;
-}
-
+  export type VersionMetaDocumentData = {
+    android: VersionMeta;
+    ios: VersionMeta;
+  }
+  
+  export type VersionMeta = {
+    minimumVersion: string;
+    latestVersion: string;
+    currentMajor: number;
+  }
 export type OnboardingMetaDocumentData = {
   onboardingTipId: string;
 }
