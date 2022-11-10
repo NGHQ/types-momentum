0003de9e5d4dcc8a0607007ca75e7030e01d9a0c
laffed <roarklaffed@gmail.com>
2022-11-09 19:24:54 -0600

    backwards compat version meta

--- src/types/models/meta.ts
+++ src/types/models/meta.ts
@@ -1,16 +1,17 @@
 export type MetaDocumentId = |
   'version' | 'onboarding'
 
-  export type VersionMetaDocumentData = {
-    android: VersionMeta;
-    ios: VersionMeta;
-  }
+export type VersionMetaDocumentData = {
+  android: VersionMeta;
+  ios: VersionMeta;
+} & VersionMeta; // DEPRECATED extending VersionMeta kept for backwards compat
   
-  export type VersionMeta = {
-    minimumVersion: string;
-    latestVersion: string;
-    currentMajor: number;
-  }
+export type VersionMeta = {
+  minimumVersion: string;
+  latestVersion: string;
+  currentMajor: number;
+}
+
 export type OnboardingMetaDocumentData = {
   onboardingTipId: string;
 }
